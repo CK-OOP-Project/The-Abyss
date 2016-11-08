3cb3ebecf3f1f9b0cc5cfa276433882959313c12
steamb23 <steamb23@gmail.com>
2016-11-08 13:30:51 +0900

    Equp -> Equip 오타 수정

---
+++ The Abyss/EquipItem.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "EquipItem.h"
+
+
+EquipItem::EquipItem(std::shared_ptr<std::string> name)
+	:Item(name)
+{
+}
+
+
+EquipItem::~EquipItem()
+{
+}
---
+++ The Abyss/EquipItem.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Item.h"
+enum class EquipType
+{
+	Weapon,
+	Shield,
+	Head,
+	Body,
+	Necklace,
+	Accessory
+};
+class EquipItem :
+	public Item
+{
+	
+public:
+	EquipItem(std::shared_ptr<std::string> name);
+	virtual ~EquipItem();
+};
+
