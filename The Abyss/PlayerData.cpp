cd1958592d4b781e8f84b2e7fecc3ca8af73ab29
steamb23 <steamb23@gmail.com>
2016-11-08 15:24:51 +0900

    PlayerData 메서드 구현

--- The Abyss/PlayerData.cpp
+++ The Abyss/PlayerData.cpp
@@ -10,3 +10,48 @@ PlayerData::PlayerData()
 PlayerData::~PlayerData()
 {
 }
+
+int PlayerData::GetHP()
+{
+	int result = this->GetStatus()->constitution;
+	for (int i = 0; i < 6; i++)
+	{
+		result += this->equipItems[i]->GetProperty()->constitution;
+	}
+	return result;
+}
+
+int PlayerData::GetMP()
+{
+	int result = this->GetStatus()->intelligence;
+	for (int i = 0; i < 6; i++)
+	{
+		result += this->equipItems[i]->GetProperty()->intelligence;
+	}
+	return result;
+}
+
+int PlayerData::GetStrikingPower()
+{
+	int result = this->GetStatus()->intelligence;
+	for (int i = 0; i < 6; i++)
+	{
+		result += this->equipItems[i]->GetProperty()->strength;
+	}
+	return result;
+}
+
+int PlayerData::GetDefensivePower()
+{
+	return 0;
+}
+
+int PlayerData::GetAccuracy()
+{
+	int result = this->GetStatus()->dexterity;
+	for (int i = 0; i < 6; i++)
+	{
+		result += this->equipItems[i]->GetProperty()->dexterity;
+	}
+	return result;
+}
