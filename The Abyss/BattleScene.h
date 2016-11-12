ffaa0da4809ecd52afad96084f1f56b077f787eb
황성준 <tjdwns960@naver.com>
2016-11-12 17:29:17 +0900

    배틀시스템 추가,아이템습득 시작

---
+++ The Abyss/BattleScene.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<memory>
+#include "Scene.h"
+#include "Battler.h"
+class BattleScene :
+	public Scene
+{
+
+public:
+	BattleScene(std::weak_ptr<Game> game,std::shared_ptr<Battler>battler1,std::shared_ptr<Battler>battler2);
+	virtual ~BattleScene();
+	virtual void Run();
+	void Battle(); 
+	void ItemGet();
+private:
+	std::shared_ptr<Battler> battler1, battler2;
+};
+
