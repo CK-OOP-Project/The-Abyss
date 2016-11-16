bb0441ca034d3be554973a1208ef02684678616e
steamb23 <steamb23@gmail.com>
2016-11-16 12:00:35 +0900

    Status 생성자 추가

--- The Abyss/Status.cpp
+++ The Abyss/Status.cpp
@@ -1,3 +1,12 @@
 #include "stdafx.h"
 #include "Status.h"
 
+Status::Status()
+	:Status(0,0,0,0)
+{
+}
+
+Status::Status(int strength, int intelligence, int dexterity, int constitution)
+	:strength(strength),intelligence(intelligence), dexterity(dexterity), constitution(constitution)
+{
+}
--- The Abyss/Status.h
+++ The Abyss/Status.h
@@ -2,6 +2,8 @@
 class Status
 {
 public:
+	Status();
+	Status(int strength, int intelligence, int dexterity, int constitution);
 	int strength;
 	int intelligence;
 	int dexterity;
