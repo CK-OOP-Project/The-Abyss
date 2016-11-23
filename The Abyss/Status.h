9f8615958fd98c99deb8fc9c2a53343b23d28053
steamb23 <steamb23@gmail.com>
2016-11-23 19:23:28 +0900

    Status 주석 달아놓음

--- The Abyss/Status.h
+++ The Abyss/Status.h
@@ -4,9 +4,13 @@ class Status
 public:
 	Status();
 	Status(int strength, int intelligence, int dexterity, int constitution);
+	// 공격력
 	int strength;
+	// 마력
 	int intelligence;
+	// 민첩성
 	int dexterity;
+	// 생명력
 	int constitution;
 };
 
