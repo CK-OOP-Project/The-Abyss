666f25e4c1454a7f0f2a52119f7a6d8b5ff9265d
steamb23 <steamb23@gmail.com>
2016-11-30 22:36:06 +0900

    장비 능력치 반영

--- The Abyss/PlayerData.cpp
+++ The Abyss/PlayerData.cpp
@@ -4,7 +4,7 @@
 
 PlayerData::PlayerData()
 {
-	status = std::make_shared<Status>();
+    status = std::make_shared<Status>();
 }
 
 
@@ -14,69 +14,74 @@ PlayerData::~PlayerData()
 
 std::shared_ptr<std::string> PlayerData::GetName()
 {
-	return std::make_shared<std::string>("플레이어");
+    return std::make_shared<std::string>("플레이어");
 }
 
 int PlayerData::GetHP()
 {
-	int result = this->GetStatus()->constitution;
-	//for (int i = 0; i < 6; i++)
-	//{
-	//	result += this->equipItems[i]->GetProperty()->constitution;
-	//}
-	return result;
+    int result = this->GetStatus()->constitution;
+    for (int i = 0; i < 6; i++)
+    {
+        if (this->equipItems[i] != nullptr)
+            result += this->equipItems[i]->GetProperty()->constitution;
+    }
+    return result;
 }
 
 int PlayerData::GetMP()
 {
-	int result = this->GetStatus()->intelligence;
-	//for (int i = 0; i < 6; i++)
-	//{
-	//	result += this->equipItems[i]->GetProperty()->intelligence;
-	//}
-	return result;
+    int result = this->GetStatus()->intelligence;
+    for (int i = 0; i < 6; i++)
+    {
+        if (this->equipItems[i] != nullptr)
+            result += this->equipItems[i]->GetProperty()->intelligence;
+    }
+    return result;
 }
 
 int PlayerData::GetStrikingPower()
 {
-	int result = this->GetStatus()->intelligence;
-	//for (int i = 0; i < 6; i++)
-	//{
-	//	result += this->equipItems[i]->GetProperty()->strength;
-	//}
-	return result;
+    int result = this->GetStatus()->intelligence;
+    for (int i = 0; i < 6; i++)
+    {
+        if (this->equipItems[i] != nullptr)
+            result += this->equipItems[i]->GetProperty()->strength;
+    }
+    return result;
 }
 
 int PlayerData::GetDefensivePower()
 {
-	int result = 0;
-	//for (int i = 0; i < 6; i++)
-	//{
-	//	result = this->equipItems[i]->GetDefensivePower();
-	//}
-	return result;
+    int result = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        if (this->equipItems[i] != nullptr)
+            result += this->equipItems[i]->GetDefensivePower();
+    }
+    return result;
 }
 
 int PlayerData::GetAccuracy()
 {
-	int result = this->GetStatus()->dexterity;
-	//for (int i = 0; i < 6; i++)
-	//{
-	//	result += this->equipItems[i]->GetProperty()->dexterity;
-	//}
-	return result;
+    int result = this->GetStatus()->dexterity;
+    for (int i = 0; i < 6; i++)
+    {
+        if (this->equipItems[i] != nullptr)
+            result += this->equipItems[i]->GetProperty()->dexterity;
+    }
+    return result;
 }
 
 int PlayerData::GetCurrentHP()
 {
-	if (currentHP > GetHP())
-		currentHP = GetHP();
-	return currentHP;
+    if (currentHP > GetHP())
+        currentHP = GetHP();
+    return currentHP;
 }
 
 void PlayerData::SetCurrentHP(int value)
 {
-	if (value > GetHP())
-		value = GetHP();
-	currentHP = value;
+    if (value > GetHP())
+        value = GetHP();
+    currentHP = value;
 }
